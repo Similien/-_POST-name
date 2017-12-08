ccb6ed80a83073823620d3d7d18f787d0be26116
Dev <futdev86@gmail.com>
2017-12-07 22:32:24 -0500

    first stage

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Forms</title>
+  </head>
+  <body>
+    <form action="testerpage.php" method="post">
+      <label>Name:</label> <input type="text" name="name">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+  </body>
+</html>
